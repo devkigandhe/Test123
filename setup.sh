e0a4632401d8914e38460dd11790ebb7e43db606
devkigandhe <devkigandhe@gmail.com>
2022-02-25 18:08:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 npm install -g newman newman-reporter-htmlextra
-newman run "/collection/Test Collection.postman_collection.json" -e "/collection/Test Env.postman_environment.json" -r htmlextra --reporter-htmlextra-export "/collection/reports.html" --env-var pageSize=$myPageSize
+newman run "/collection/Test Collection.postman_collection.json" -e "/collection/Test Env.postman_environment.json" -r htmlextra --reporter-htmlextra-export "/temp/reports.html" --env-var pageSize=$myPageSize
